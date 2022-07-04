906602932ebd9c28ce4ab9caeef964d31c96bc02
srivastavajatin12 <104339628+srivastavajatin12@users.noreply.github.com>
2022-07-04 16:47:29 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,4 +1,4 @@
 sudo apt update -y
 sudo apt -y install apache2
 git clone https://github.com/srivastavajatin12/beginner-html-site-scripted.git
-mv beginner-html-site-scripted/* /var/www/html
+mv beginner-html-site-scripted/* /var/www/html/
