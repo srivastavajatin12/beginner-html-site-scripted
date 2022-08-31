6ce3917775ca46ce1765cf28489e40cf87da73e4
srivastavajatin12 <104339628+srivastavajatin12@users.noreply.github.com>
2022-08-31 17:21:55 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -16,18 +16,18 @@ def decode_key(key):
 
 private_key = config.require_secret('Id.pem').apply(decode_key)
 
-virtualprivatecloud = aws.ec2.Vpc("devopsjunc-vpc", cidr_block="10.0.0.0/24")
+virtualprivatecloud = aws.ec2.Vpc("devopsjunc-vpc", cidr_block="10.0.0.0/16")
 
 publicsubnet = aws.ec2.Subnet("devopsjunc-public-subnet",
     vpc_id=virtualprivatecloud.id,
-    cidr_block= "10.0.0.0/24",
+    cidr_block= "10.0.0.0/16",
     map_public_ip_on_launch=True,
     tags={
         "Name": "devopsjunc-public-subnet",
     })
 privatesubnet = aws.ec2.Subnet("devopsjunc-private-subnet",
     vpc_id=virtualprivatecloud.id,
-    cidr_block= "10.1.0.0/24",
+    cidr_block= "10.1.0.0/16",
     map_public_ip_on_launch=False,
     tags={
         "Name": "devopsjunc-private-subnet",
