88dda8b3342d7e9b40bef1dbe21794bcbec12d6d
srivastavajatin12 <104339628+srivastavajatin12@users.noreply.github.com>
2022-08-31 16:04:09 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -58,18 +58,18 @@ rds_sg = aws.rds.SecurityGroup("rds_sg",
 )
 
 
-#rds_server = aws.rds.Instance("db-server",
- #   allocated_storage=10,
-  #  engine="mysql",
-   # engine_version="5.7",
-    #instance_class="db.t3.micro",
-    #db_name="mydb",
-    #parameter_group_name="default.mysql5.7",
-    #password="database",
-    #skip_final_snapshot=True,
-    #username="database",
-    #vpc_security_group_ids=[rds_sg.id],
-#)
+rds_server = aws.rds.Instance("db-server",
+    allocated_storage=10,
+    engine="mysql",
+    engine_version="5.7",
+    instance_class="db.t3.micro",
+    db_name="mydb",
+    parameter_group_name="default.mysql5.7",
+    password="database",
+    skip_final_snapshot=True,
+    username="database",
+    vpc_security_group_ids=[rds_sg.id],
+)
 
 bucket = aws.s3.Bucket("bucket",
     acl="public-read",
