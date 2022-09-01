18260e93f556fe92d8542f825f8a2ca09bfba824
srivastavajatin12 <104339628+srivastavajatin12@users.noreply.github.com>
2022-09-01 19:27:49 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -28,7 +28,7 @@ publicsubnet = aws.ec2.Subnet("devopsjunc-public-subnet",
     })
 privatesubnet = aws.ec2.Subnet("devopsjunc-private-subnet",
     vpc_id=virtualprivatecloud.id,
-    cidr_block= "10.0.1.0/24",
+    cidr_block= "172.0.1.0/24",
     map_public_ip_on_launch=False,
     availability_zone = "us-east-1b",                           
     tags={
@@ -36,7 +36,7 @@ privatesubnet = aws.ec2.Subnet("devopsjunc-private-subnet",
     })
 privatesubnet2 = aws.ec2.Subnet("devopsjunc-private-subnet2",
     vpc_id=virtualprivatecloud.id,
-    cidr_block= "10.0.2.0/24",
+    cidr_block= "172.0.2.0/24",
     map_public_ip_on_launch=False,
     availability_zone = "us-east-1a",                           
     tags={
