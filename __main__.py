bfef5aef750186eabf9ed0ff2a07946c3157c4a7
srivastavajatin12 <104339628+srivastavajatin12@users.noreply.github.com>
2022-09-02 10:16:39 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -74,7 +74,7 @@ server = aws.ec2.Instance('web-server',
     key_name='Id',
     vpc_security_group_ids=[group.id],# reference the security group resource above
     subnet_id=publicsubnet.id,
-    iam_instance_profile = "AmazonSSMRoleForInstancesQuickSetup"
+    iam_instance_profile = "AmazonSSMRoleForInstancesQuickSetup",
     user_data = """#!/bin/bash
                  set -ex
                  cd /tmp
