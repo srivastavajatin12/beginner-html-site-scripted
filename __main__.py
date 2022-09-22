08ade8b4904f5099c5354cee2b80373e7f934a62
srivastavajatin12 <104339628+srivastavajatin12@users.noreply.github.com>
2022-09-22 19:49:02 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -51,24 +51,24 @@ group = aws.ec2.SecurityGroup('web-sg',
     description='Enable HTTP access',
     egress = [
           {
-           cidr_blocks      = [ "0.0.0.0/0", ]
-           description      = ""
-           from_port        = 0
-           ipv6_cidr_blocks = []
-           prefix_list_ids  = []
-           protocol         = "-1"
-           security_groups  = []
-           self             = false
-           to_port          = 0
+           cidr_blocks      : [ "0.0.0.0/0", ]
+           description      : ""
+           from_port        : 0
+           ipv6_cidr_blocks : []
+           prefix_list_ids  : []
+           protocol         : "-1"
+           security_groups  : []
+           self             : false
+           to_port          : 0
           }
        ]
       ingress = [
           {
-           cidr_blocks      = [ "0.0.0.0/0", ]
-           description      = ""
-           from_port        = 22
-           ipv6_cidr_blocks = []
-           prefix_list_ids  = []
+           cidr_blocks      : [ "0.0.0.0/0", ]
+           description      : ""
+           from_port        : 22
+           ipv6_cidr_blocks : []
+           prefix_list_ids  : []
           }
       ]
      # vpc_id=virtualprivatecloud.id
